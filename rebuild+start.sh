be9b73ee2007cd0cf021830c5d19a1b63d1f3a5a
Михаил Евпатов <mihailevpatov@rfc1918.private.ip.localhost>
2023-03-31 18:55:11 +0300

    update

--- rebuild+start.sh
+++ rebuild+start.sh
@@ -13,5 +13,5 @@ cmake -G "Unix Makefiles"  -B ./build && cmake --build ./build
 
 #-D CMAKE_C_COMPILER=clang -D CMAKE_CXX_COMPILER=clang++
 
-./build/bin/vk-test_physx.exe
-./build/bin/vk-test_vk.exe
+./build/bin/vk-test_physx
+./build/bin/vk-test_vk
