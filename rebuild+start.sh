9371626f2dacd46360d759698923e03268bf9432
asgrd <asgrd@rfc1918.private.ip.localhost>
2023-04-21 16:55:02 +0300

    update

--- rebuild+start.sh
+++ rebuild+start.sh
@@ -9,8 +9,8 @@ fi
 
 cmake -G "Unix Makefiles" -B ./build -DCMAKE_EXPORT_COMPILE_COMMANDS=1 && cmake --build ./build
 
-./build/bin/vk-test_physx
-./build/bin/vk-test_sdl
-
-./build/bin/vk-test_vk_sdl_test_1
-./build/bin/vk-test_vk_sdl_test_2
+for app in "$@" 
+do
+    echo "Starting ./build/bin/vk-test_$app";
+    ./build/bin/vk-test_$app
+done
