67c6860ae2f6c9a3aa471c8656793c695b00ac3a
maxfps11 <super.vbif201@ya.ru>
2023-04-25 00:45:15 +0300

    update

--- src/vk_sdl_test_1/main.cpp
+++ src/vk_sdl_test_1/main.cpp
@@ -20,10 +20,10 @@ int main() {
     SDL_Init(SDL_INIT_EVERYTHING);
     window = SDL_CreateWindow(WINDOW_NAME.c_str(),SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,800,600,SDL_WINDOW_VULKAN | SDL_WINDOW_SHOWN);
 
-    uint32_t extensionCount = 0;
-    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+    //uint32_t extensionCount = 0;
+    //vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
 
-    std::cout << extensionCount << " extensions supported\n";
+    //std::cout << extensionCount << " extensions supported\n";
 
     SDL_Event event;
     bool running = true;
--- src/vk_sdl_test_2/main.cpp
+++ src/vk_sdl_test_2/main.cpp
@@ -13,7 +13,7 @@ using namespace std;
 
 #include <vulkan/vulkan.h>
 
-#include <glm/glm.hpp>
+//#include <glm/glm.hpp>
 
 SDL_Window *window;
 const std::string WINDOW_NAME = "SDL2 Vulkan Test";
@@ -22,9 +22,9 @@ int main() {
     SDL_Init(SDL_INIT_EVERYTHING);
     window = SDL_CreateWindow(WINDOW_NAME.c_str(),SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,800,600,SDL_WINDOW_VULKAN | SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
    
-    uint32_t extensionCount = 0;
-    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
-    cout << "extensionCount: " << extensionCount << endl;
+    //uint32_t extensionCount = 0;
+    //vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+    //cout << "extensionCount: " << extensionCount << endl;
 
     // glm::mat4 matrix;
     // glm::vec4 vec;
