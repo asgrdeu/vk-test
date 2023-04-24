9e2d9d141c965e7ad6e1f74ccb7f13c15d2e5730
zvartOne <zvart1one@gmail.com>
2023-04-24 18:20:45 +0300

    update

--- src/vk_sdl_test_2/main.cpp
+++ src/vk_sdl_test_2/main.cpp
@@ -2,7 +2,9 @@
     SDL2 Vulkan Test
 */
 
+#include <SDL_video.h>
 #if defined (_WIN32)
+#define SDL_MAIN_HANDLED
 #include <SDL.h>
 #include <SDL_log.h>
 
@@ -24,36 +26,13 @@ using namespace std;
 SDL_Window *window;
 const std::string WINDOW_NAME = "vk_sdl_test_2";
 
-int main(int argc, char *argv[]) {
+int main() {
     SDL_Init(SDL_INIT_EVERYTHING);
-    window = SDL_CreateWindow(WINDOW_NAME.c_str(),SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,800,600,SDL_WINDOW_VULKAN | SDL_WINDOW_SHOWN);
-
-#if defined (_WIN32)
-    // AllocConsole();
-
-    // HANDLE handle_out = GetStdHandle(STD_OUTPUT_HANDLE);
-    // int hCrt = _open_osfhandle((long) handle_out, _O_TEXT);
-    // FILE* hf_out = _fdopen(hCrt, "w");
-    // setvbuf(hf_out, NULL, _IONBF, 1);
-    // *stdout = *hf_out;
-    
-    printf_s("const char *const Format, ...", "nhbybyihbiub");
-
-    // HANDLE handle_in = GetStdHandle(STD_INPUT_HANDLE);
-    // hCrt = _open_osfhandle((long) handle_in, _O_TEXT);
-    // FILE* hf_in = _fdopen(hCrt, "r");
-    // setvbuf(hf_in, NULL, _IONBF, 128);
-    // *stdin = *hf_in;
-
-    //freopen( "output.txt", "w", stdout );
-    // freopen_s( "error.txt", "w", stderr );
-
+    window = SDL_CreateWindow(WINDOW_NAME.c_str(),SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,800,600,SDL_WINDOW_VULKAN | SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
+   
     uint32_t extensionCount = 0;
     vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
-
-    //cout << extensionCount << " extensions supported\n";
-    //fclose(stdout);
-#endif
+    cout << "extensionCount: " << extensionCount << endl;
 
     SDL_Event event;
     bool running = true;
