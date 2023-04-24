5a7295ab30795f1eeec298ac9686a5d10261d19c
zvartOne <zvart1one@gmail.com>
2023-04-25 00:40:07 +0300

    triangle

---
+++ src/vk_sdl_test_2/triangl.h
@@ -0,0 +1,30 @@
+#include <vulkan/vulkan.h>
+
+#include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+#include <vulkan/vulkan_core.h>
+
+class TriangleApplication {
+    public:
+        void run(){
+            initVulkan();
+            mainLoop();
+            cleanUp();
+        }
+        
+    private:
+        void initVulkan(){
+
+        }
+        
+        void mainLoop(){
+        
+        }
+        
+        void cleanUp(){
+
+        }
+
+            
+};
