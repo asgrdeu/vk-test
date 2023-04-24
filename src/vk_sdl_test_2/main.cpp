c0b49c3af043d0e93a96174ebe3c94a867fcbf61
zvartOne <zvart1one@gmail.com>
2023-04-24 16:24:27 +0300

    update

--- src/vk_sdl_test_2/main.cpp
+++ src/vk_sdl_test_2/main.cpp
@@ -1,19 +1,17 @@
-#include <SDL_log.h>
-#include <iostream>
+#if defined (_WIN32)
+#include <windows.h>
+#include <SDL.h>
+#include <io.h>
 #include <stdio.h>
 #include <fcntl.h>
-
-using namespace std;
-
-#if defined (_WIN32)
-#include <SDL.h>
-
-#include <Windows.h>
-#include <io.h>
 
 #else
 #include <SDL2/SDL.h>
 #endif
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
 
 #include <vulkan/vulkan.h>
 
@@ -28,29 +26,35 @@ int main(int argc, char **argv) {
     SDL_Init(SDL_INIT_EVERYTHING);
     window = SDL_CreateWindow(WINDOW_NAME.c_str(),SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,800,600,SDL_WINDOW_VULKAN | SDL_WINDOW_SHOWN);
 
+    
+
+    
+
+#if defined (_WIN32)
+    // AllocConsole();
+
+    // HANDLE handle_out = GetStdHandle(STD_OUTPUT_HANDLE);
+    // int hCrt = _open_osfhandle((long) handle_out, _O_TEXT);
+    // FILE* hf_out = _fdopen(hCrt, "w");
+    // setvbuf(hf_out, NULL, _IONBF, 1);
+    // *stdout = *hf_out;
+    
+    printf_s("const char *const Format, ...", "nhbybyihbiub");
+
+    // HANDLE handle_in = GetStdHandle(STD_INPUT_HANDLE);
+    // hCrt = _open_osfhandle((long) handle_in, _O_TEXT);
+    // FILE* hf_in = _fdopen(hCrt, "r");
+    // setvbuf(hf_in, NULL, _IONBF, 128);
+    // *stdin = *hf_in;
+
+    //freopen( "output.txt", "w", stdout );
+    // freopen_s( "error.txt", "w", stderr );
+
     uint32_t extensionCount = 0;
     vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
 
-    std::cout << extensionCount << " extensions supported\n";
-
-    SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Hello world!");
-
-#if defined (_WIN32)
-    AllocConsole();
-
-    HANDLE handle_out = GetStdHandle(STD_OUTPUT_HANDLE);
-    int hCrt = _open_osfhandle((long) handle_out, _O_TEXT);
-    FILE* hf_out = _fdopen(hCrt, "w");
-    setvbuf(hf_out, NULL, _IONBF, 1);
-    *stdout = *hf_out;
-
-    HANDLE handle_in = GetStdHandle(STD_INPUT_HANDLE);
-    hCrt = _open_osfhandle((long) handle_in, _O_TEXT);
-    FILE* hf_in = _fdopen(hCrt, "r");
-    setvbuf(hf_in, NULL, _IONBF, 128);
-    *stdin = *hf_in;
-
-    printf("HIIIIIII");
+    //cout << extensionCount << " extensions supported\n";
+    //fclose(stdout);
 #endif
 
     SDL_Event event;
