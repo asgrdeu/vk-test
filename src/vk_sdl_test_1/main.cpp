d112b9fa791c2b4825295ace4b40694c946bb361
asgrd <asgrd@rfc1918.private.ip.localhost>
2023-04-25 15:38:29 +0300

    vk-instance-creation

--- src/vk_sdl_test_1/main.cpp
+++ src/vk_sdl_test_1/main.cpp
@@ -8,6 +8,7 @@
 
 // SDL2 includes
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_vulkan.h>
 
 // Vulkan includes
 #include <vulkan/vulkan.h>
@@ -34,29 +35,63 @@ private:
   void initWindow() {
     SDL_Init(SDL_INIT_EVERYTHING);
 
+    const string WINDOW_NAME = ENGINE_NAME + " - " + APPLICATION_NAME;
+
     window = SDL_CreateWindow(WINDOW_NAME.c_str(), SDL_WINDOWPOS_CENTERED,
                               SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT,
                               SDL_WINDOW_VULKAN | SDL_WINDOW_SHOWN);
   };
 
-  void initVulkan(){};
+  void initVulkan() { createInstance(); };
 
   void mainLoop() {
     while (event.type != SDL_QUIT)
       SDL_PollEvent(&event);
   };
 
-  void cleanup(){
+  void cleanup() {
     SDL_DestroyWindow(window);
     window = nullptr;
     SDL_Quit();
   };
 
+  void createInstance() {
+    applicationInfo = {};
+    applicationInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    applicationInfo.pNext = nullptr;
+    applicationInfo.pApplicationName = APPLICATION_NAME.c_str();
+    applicationInfo.applicationVersion = VK_MAKE_VERSION(0, 0, 1);
+    applicationInfo.pEngineName = ENGINE_NAME.c_str();
+    applicationInfo.engineVersion = VK_MAKE_VERSION(0, 0, 1);
+    applicationInfo.apiVersion = VK_API_VERSION_1_3;
+
+    instanceCreateInfo = {};
+    instanceCreateInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    instanceCreateInfo.pNext = nullptr;
+    instanceCreateInfo.flags = 0;
+    instanceCreateInfo.pApplicationInfo = &applicationInfo;
+    instanceCreateInfo.enabledLayerCount = 0;
+    instanceCreateInfo.ppEnabledLayerNames = nullptr;
+    instanceCreateInfo.enabledExtensionCount = 0;
+    instanceCreateInfo.ppEnabledExtensionNames = nullptr;
+  }
+
+  // SDL2 Window
   SDL_Window *window;
   SDL_Event event;
-  const string WINDOW_NAME = "vk_sdl_test_1";
   const uint32_t WIDTH = 800;
   const uint32_t HEIGHT = 600;
+
+  // Vulkan
+  VkInstance instance;
+  VkApplicationInfo applicationInfo;
+  VkInstanceCreateInfo instanceCreateInfo;
+
+  // Engine
+  const string ENGINE_NAME = "Vulkan Engine";
+
+  // Application
+  const string APPLICATION_NAME = "Hello Triangle";
 };
 
 int main() {
@@ -68,6 +103,6 @@ int main() {
     std::cerr << e.what() << std::endl;
     return EXIT_FAILURE;
   }
-  
+
   return EXIT_SUCCESS;
 }
