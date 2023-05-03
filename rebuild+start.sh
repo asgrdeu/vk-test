57dcfd7886db876102ec1c2e6e4495f2fdefce3a
maxfps11 <super.vbif201@ya.ru>
2023-05-03 19:33:10 +0300

    update

--- rebuild+start.sh
+++ rebuild+start.sh
@@ -4,13 +4,22 @@ if [ ! -d "./build" ]; then
     mkdir build
 else
     rm -rf ./build
+    rm -rf ./.cache
     mkdir build
 fi
 
-cmake -G "Unix Makefiles" -B ./build -DCMAKE_EXPORT_COMPILE_COMMANDS=1 && cmake --build ./build -j8
-
-for app in "$@" 
+while getopts p:r: flag
 do
-    echo "Starting ./build/bin/vk-test_$app";
-    ./build/bin/vk-test_$app
+    case "${flag}" in
+        p) projects=${OPTARG};;
+        r) run=${OPTARG};;
+    esac
+done
+
+cmake -G "Unix Makefiles" -B ./build -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DENABLE_PROJECT_LIST=$projects && cmake --build ./build -j8
+
+for app in "$run" 
+do
+    echo "Starting ./build/bin/$app";
+    ./build/bin/$app
 done
