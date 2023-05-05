cf6b49db1f3b7dd9d72f30b7b1b2b30e9dbebb81
maxfps11 <super.vbif201@ya.ru>
2023-05-05 23:56:49 +0300

    sdl-refactor+cleanup

--- sdl/src/main.cpp
+++ sdl/src/main.cpp
@@ -9,30 +9,28 @@
 #include <SDL2/SDL.h>
 
 #include <iostream>
+
 using namespace std;
 
 SDL_Window *window;
 const std::string WINDOW_NAME = "SDL2 Vulkan application";
 
 int main(int argc, char **argv) {
-  SDL_Init(SDL_INIT_EVERYTHING);
+  if (SDL_Init(SDL_INIT_VIDEO))
+    cout << "Failed to initialize SDL2 with:  " << SDL_GetError() << endl;
+
   window = SDL_CreateWindow(WINDOW_NAME.c_str(), SDL_WINDOWPOS_CENTERED,
                             SDL_WINDOWPOS_CENTERED, 800, 600,
                             SDL_WINDOW_VULKAN | SDL_WINDOW_SHOWN);
 
+  if (window)
+    cout << "Failed to create window with: " << SDL_GetError() << endl;
+
   SDL_Event event;
-  bool running = true;
-  while (running) {
-    while (SDL_PollEvent(&event)) {
-      if (event.type == SDL_QUIT) {
-        running = false;
-      }
-    }
-  }
+  while (event.type != SDL_QUIT)
+    SDL_PollEvent(&event);
 
   SDL_DestroyWindow(window);
-  window = nullptr;
-
   SDL_Quit();
   return 0;
 }
